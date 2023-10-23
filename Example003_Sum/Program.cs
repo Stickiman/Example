9015d45435ee35c2d70e8d905d8abfb6deac59a0
Ilya Steputenko <Stickimaniya@gmail.com>
2023-10-23 14:01:57 +0700

    add Example 005 If - Else

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(,10); // 1 2 3 ... 9
+int numberB = new Random().Next(,10);
 Console.WriteLine (numberA + numberB);
 int result = numberA + numberB;
 Console.WriteLine ( result);
---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя прользователя");
+string username - console.ReadLine();
+
+if (username.ToLower() == "Маша")
+    {
+        Console.WriteLine("Ура, это же Маша")
+    }
+       {
+        Console.Write("Привет, ")
+    }
